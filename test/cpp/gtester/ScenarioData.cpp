32fe2db25c24c857f34e70291b98e02664162f49
lwillem <lander.willem@uantwerpen.be>
2019-10-07 17:19:52 +0200

    [GTEST] Updated reference value of influenza scenario.

--- test/cpp/gtester/ScenarioData.cpp
+++ test/cpp/gtester/ScenarioData.cpp
@@ -39,7 +39,7 @@ tuple<ptree, unsigned int, double> ScenarioData::Get(string tag)
                                             : RunConfigManager::Create("TestsMeasles");
 
         const map<string, unsigned int> targets_default = {
-            {"influenza_a", 535000U}, {"influenza_b", 0U}, {"influenza_c", 5U}, {"measles_16", 275000U},
+            {"influenza_a", 534000U}, {"influenza_b", 0U}, {"influenza_c", 5U}, {"measles_16", 275000U},
             {"measles_26", 600000U},  {"r0_0", 1200U},     {"r0_4", 4500U},     {"r0_8", 18000U},
             {"r0_12", 45000U},        {"r0_16", 80000U}};
 
