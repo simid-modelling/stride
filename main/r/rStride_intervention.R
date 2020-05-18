93890a67bc2346e877d9f466b29e761b9cf0f2ec
lwillem <lander.willem@uantwerpen.be>
2020-05-18 10:41:18 +0200

    [SCENARIO] Updated rStride_intervention.R script with baseline settings for the scenario analysis on May 18th.

--- main/r/rStride_intervention.R
+++ main/r/rStride_intervention.R
@@ -41,39 +41,37 @@ dir_postfix <- '_int'
 #names(xmlToList('./config/run_default.xml'))
 
 # set the number of realisations per configuration set
-num_seeds  <- 2
+num_seeds  <- 10
 
 # add parameters and values to combine in a full-factorial grid
-exp_design <- expand.grid(r0                            = seq(3.5,3.5,0.1),
-                          num_days                      = 170,
+exp_design <- expand.grid(r0                            = seq(3.4,3.4,0.1),
+                          num_days                      = 196,
                           rng_seed                      = seq(num_seeds),
-                          num_participants_survey       = 3000,
-                          num_infected_seeds            = c(900),
-                          disease_config_file           = "disease_covid19_child.xml",
-                          population_file               = "pop_belgium3000k_c500_teachers_censushh.csv",
+                          num_participants_survey       = 300,
+                          num_infected_seeds            = 750,
+                          disease_config_file           = "disease_covid19_age.xml",
+                          population_file               = c("pop_belgium11M_c500_teachers_censushh.csv"),
                           age_contact_matrix_file       = "contact_matrix_flanders_conditional_teachers.xml",
                           start_date                    = c('2020-02-17'),
-                          # holidays_file                 = 'calendar_belgium_2020_covid19_exit_school_adjusted.json',
-                          holidays_file                 = 'calendar_belgium_2020_covid19_may_k12school.json',
-                          # holidays_file                 = c("calendar_belgium_2020_covid19_may_preschool.json",
-                          #                                   "calendar_belgium_2020_covid19_may_primary_school.json",
-                          #                                   "calendar_belgium_2020_covid19_may_secondary_school.json",
-                          #                                   "calendar_belgium_2020_covid19_may_workplace.json"),
-                           school_system_adjusted        = 1,
+                          holidays_file                 = 'calendar_belgium_2020_covid19_exit_school_adjusted.json',
+                          school_system_adjusted        = 1,
                           telework_probability          = c(0),
                           cnt_reduction_workplace       = c(0.8),
                           cnt_reduction_other           = c(0.85),
                           compliance_delay_workplace    = c(6),
                           compliance_delay_other        = c(6),
                           num_daily_imported_cases      = c(0),
-                          cnt_reduction_workplace_exit  = c(0.4),
-                          cnt_reduction_other_exit      = 0.75,
+                          cnt_reduction_workplace_exit  = seq(0.2,0.6,0.2),
+                          cnt_reduction_other_exit      = seq(0.7,0.8,0.1),
                           cnt_reduction_school_exit     = 0.5,
-                          cnt_reduction_intergeneration = c(0.9),
+                          cnt_reduction_intergeneration = 0.9,
                           cnt_reduction_intergeneration_cutoff = 65,
-                          detection_probability          = 0,
+                          detection_probability          = 0.5,
                           case_finding_efficency         = 0.7,
-                          case_finding_capacity          = c(2000),
+                          case_finding_capacity          = 10000, # no limit at this stage
+                          delay_contact_tracing          = 3,
+                          delay_testing                  = 1,
+                          test_false_negative            = 0.1,
                           stringsAsFactors = F)
 
 # check period
@@ -88,9 +86,7 @@ dim(exp_design)
 ## RUN rSTRIDE                  ####
 ################################## #
 project_dir <- run_rStride(exp_design               = exp_design,
-                           dir_postfix              = dir_postfix, 
-                           remove_run_output        = TRUE,
-                           ignore_stdout            = TRUE)
+                           dir_postfix              = dir_postfix)
 
 
 ############################# #
