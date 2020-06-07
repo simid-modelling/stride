22b4fdb6568af7683b96f1e97e73aaf288f67f94
lwillem <lander.willem@uantwerpen.be>
2020-06-07 11:38:06 +0200

    Updated the intervention_baseline parameters and included disease file with age-specific infectiousness (children = 1/2 adults) [ci skip]

--- main/r/rStride_intervention_baseline.R
+++ main/r/rStride_intervention_baseline.R
@@ -26,40 +26,51 @@
 ################################## #
 
 # get default parameter values to combine in a full-factorial grid
-get_exp_param_default <- function(){
-   list(r0                            = seq(3.4,3.4,0.1),
-        num_days                      = 196,
-        num_seeds                     = 10,
-        num_participants_survey       = 300,
-        num_infected_seeds            = 510,
-        disease_config_file           = "disease_covid19_age.xml",
-        population_file               = c("pop_belgium11M_c500_teachers_censushh.csv"),
-        age_contact_matrix_file       = "contact_matrix_flanders_conditional_teachers.xml",
-        start_date                    = c('2020-02-17'),
-        holidays_file                 = 'calendar_belgium_2020_covid19_exit_school_adjusted.json',
-        school_system_adjusted        = 1,
-        telework_probability          = c(0),
-        cnt_reduction_workplace       = 0.8,
-        cnt_reduction_other           = 0.85,
-        compliance_delay_workplace    = c(6),
-        compliance_delay_other        = c(6),
-        num_daily_imported_cases      = c(0),
-        cnt_reduction_workplace_exit  = seq(0.6,0.8,0.1),
-        cnt_reduction_other_exit      = c(0.7,0.8,0.85),
-        cnt_reduction_school_exit     = 0.5,
-        cnt_reduction_intergeneration = 0.9,
-        cnt_reduction_intergeneration_cutoff = 65,
-        cnt_intensity_householdCluster = 0,
-        detection_probability          = 0,
-        case_finding_efficency         = 0.7,
-        case_finding_capacity          = 10000, # no limit at this stage
-        delay_contact_tracing          = 3,
-        delay_testing                  = 1,
-        test_false_negative            = 0.1,
-        cnt_other_exit_delay           = seq(14,28,7),
-        
-        # factor for parameter estimation and fitting
-        hosp_probability_factor        = 1
-  )
+get_exp_param_default <- function(bool_child_param = FALSE){
+   out <- list(r0                            = seq(3.4,3.4,0.1),
+                num_days                      = 196,
+                num_seeds                     = 10,
+                num_participants_survey       = 300,
+                num_infected_seeds            = 510,
+                disease_config_file           = "disease_covid19_age.xml",
+                population_file               = c("pop_belgium11M_c500_teachers_censushh.csv"),
+                age_contact_matrix_file       = "contact_matrix_flanders_conditional_teachers.xml",
+                start_date                    = c('2020-02-17'),
+                holidays_file                 = 'calendar_belgium_2020_covid19_exit_school_adjusted.json',
+                school_system_adjusted        = 1,
+                telework_probability          = c(0),
+                cnt_reduction_workplace       = 0.8,
+                cnt_reduction_other           = 0.85,
+                compliance_delay_workplace    = c(6),
+                compliance_delay_other        = c(6),
+                num_daily_imported_cases      = c(0),
+                cnt_reduction_workplace_exit  = seq(0.6,0.8,0.1),
+                cnt_reduction_other_exit      = c(0.7,0.8,0.85),
+                cnt_reduction_school_exit     = 0.5,
+                cnt_reduction_intergeneration = 0.9,
+                cnt_reduction_intergeneration_cutoff = 65,
+                cnt_intensity_householdCluster = 0,
+                detection_probability          = 0,
+                case_finding_efficency         = 0.7,
+                case_finding_capacity          = 10000, # no limit at this stage
+                delay_contact_tracing          = 3,
+                delay_testing                  = 1,
+                test_false_negative            = 0.1,
+                cnt_other_exit_delay           = seq(14,21,7),
+                
+                # factor for parameter estimation and fitting
+                hosp_probability_factor        = 1
+          )
+   
+   # change parameters if childrens infectiousness is 1/2 compared to adults
+   if(bool_child_param){ #best fit on 2020-06-07
+     out$disease_config_file <- "disease_covid19_child.xml"
+     out$cnt_reduction_workplace <- 0.75
+     out$cnt_reduction_other     <- 0.90
+     out$hosp_probability_factor <- 0.85
+   }
+   
+   # return parameters
+   return(out)
 }
 
